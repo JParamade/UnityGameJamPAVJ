f8f5d434c61e9ae3870b6763e4a3276b2612e093
Jorge-Utad <jorge.duart@live.u-tad.com>
2025-03-24 21:54:02 +0100

    Detection bombs

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] LayerMask lmGround;
+    [SerializeField] LayerMask lmBomb;
     [SerializeField] float speed = 10;
     [SerializeField] float force = 100;
+    [SerializeField] float speedH;
     Rigidbody rb;
-    [SerializeField] bool isGrounded = true;
+    bool isGrounded = true;
+    bool dir = true;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -27,6 +31,26 @@ public class Player : MonoBehaviour
             rb.AddForce(force * Time.fixedDeltaTime * Vector3.up, ForceMode.Impulse);
         }
 
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit grab;
+            if(dir)
+            {
+                if(Physics.Raycast(transform.position - new Vector3(0, 0.5f, 0), Vector3.right, out grab, 1, lmBomb))
+                {
+                    print("BOOOMB");
+                }
+            }
+            else
+            {
+                if (Physics.Raycast(transform.position - new Vector3(0, 0.5f, 0), Vector3.left, out grab, 1, lmBomb))
+                {
+                    print("BOOOMB");
+                }
+            }
+        }
+
         RaycastHit hit;
         if(Physics.Raycast(transform.position, Vector3.down, out hit, 1, lmGround))
         {
@@ -36,14 +60,24 @@ public class Player : MonoBehaviour
         {
             isGrounded = false;
         }
+
         
+
     }
 
     void MoverPlayer()
     {
         Vector3 traslation = (transform.right * Input.GetAxisRaw("Horizontal")).normalized;
+        if(Input.GetAxisRaw("Horizontal") == 1)
+        {
+            dir = true;
+        } 
+        else if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            dir = false; 
+        }
+
         rb.MovePosition(transform.position + speed * Time.fixedDeltaTime * traslation);
     }
 
-    
 }
