698f01797ee5959d93a9cfb74330860f5a26f0c7
Jorge-Utad <jorge.duart@live.u-tad.com>
2025-03-24 20:53:15 +0100

    Bombas

---
+++ Assets/_Scripts/LA_BOMBA.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LA_BOMBA : MonoBehaviour
+{
+    [SerializeField] float radioExplo = 5;
+    [SerializeField] float exploForce = 500;
+    [SerializeField] float timeExplosion = 5;
+    Coroutine Booooom;
+    void Start()
+    {
+        Booooom = StartCoroutine(Explosion());
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Explosion()
+    {
+        yield return new WaitForSeconds(timeExplosion);
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radioExplo);
+        foreach (Collider coll in colliders)
+        {
+            if (coll.GetComponent<Rigidbody>() != null)
+            {
+                coll.GetComponent<Rigidbody>().AddExplosionForce(exploForce, transform.position, radioExplo, 1);
+            }
+        }
+        Destroy(this.gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, radioExplo);
+    }
+}
