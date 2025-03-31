5dea5d0d772944638b315c6f72b9305a4aebf697
JParamade <jaime.paramo@outlook.com>
2025-03-31 21:33:15 +0200

    Added Time Out Screen UI & Logic

--- Assets/_Scripts/SceneManager.cs
+++ Assets/_Scripts/SceneManager.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
-    public void PlayGame() { UnityEngine.SceneManagement.SceneManager.LoadScene("pHABLO"); }
+    public void PlayGame(string _sSceneName) { UnityEngine.SceneManagement.SceneManager.LoadScene(_sSceneName); }
     public void QuitGame() { Application.Quit(); }
 }
